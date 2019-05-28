4e10bc1e5d96b8e5d87b51003ba3ee9283495526
umeshredd <47346567+umeshredd@users.noreply.github.com>
2019-05-28 11:24:48 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://preetham:preetham@cluster0-zcefm.mongodb.net/test?retryWrites=true',
+  mongoURI: 'mongodb+srv://honey:12345@cluster0-tl3sm.gcp.mongodb.net/test?retryWrites=true',
 }
